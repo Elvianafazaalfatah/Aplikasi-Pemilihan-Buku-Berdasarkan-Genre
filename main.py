acee969dba360174ad37a059b20467b07723e933
viana <elvianafazaalfatah@student.uns.ac.id>
2024-12-07 17:23:49 +0700

    progress 4

---
+++ main.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from modules.ui import AplikasiRekomendasiBuku
+
+# Menjalankan aplikasi
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = AplikasiRekomendasiBuku(root)
+    root.mainloop()
